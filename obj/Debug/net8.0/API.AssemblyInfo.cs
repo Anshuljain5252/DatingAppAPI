3eac17e3b2ca3e4946a7fd58633beba2dc5f2713
Anshul Jain <anshul.jain@sierralivingconcepts.com>
2024-11-29 11:56:28 +0530

    add

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894c864b4d06de1464b66228b531deb8777cfa28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b5c64c29c48035483a256a92629237cd36a486")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
